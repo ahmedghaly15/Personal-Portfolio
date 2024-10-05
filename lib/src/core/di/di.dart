2093c4e44dac81a34238c73c5aa875190d2ced5c
Ahmed Ghaly <ahmedghaly0767@gmail.com>
2024-10-05 14:46:23 +0300

    Updated PersonalPortfolioApp widget

--- lib/src/core/di/di.dart
+++ lib/src/core/di/di.dart
@@ -1,6 +1,7 @@
 import 'package:get_it/get_it.dart';
 import 'package:personal_portfolio/src/core/locale/logic/locale_cubit.dart';
 import 'package:personal_portfolio/src/core/locale/logic/locale_repo.dart';
+import 'package:personal_portfolio/src/core/themes/themes_cubit.dart';
 
 final GetIt getIt = GetIt.instance;
 
@@ -17,4 +18,5 @@ void _setupForCubits() {
   getIt.registerLazySingleton<LocaleCubit>(
     () => LocaleCubit(getIt<LocaleRepo>()),
   );
+  getIt.registerLazySingleton<ThemingCubit>(() => ThemingCubit());
 }
--- lib/src/personal_portfolio_app.dart
+++ lib/src/personal_portfolio_app.dart
@@ -5,6 +5,7 @@ import 'package:personal_portfolio/src/core/di/di.dart';
 import 'package:personal_portfolio/src/core/locale/app_localizations_setup.dart';
 import 'package:personal_portfolio/src/core/locale/logic/locale_cubit.dart';
 import 'package:personal_portfolio/src/core/themes/app_themes.dart';
+import 'package:personal_portfolio/src/core/themes/themes_cubit.dart';
 import 'package:personal_portfolio/src/core/utils/app_strings.dart';
 import 'package:personal_portfolio/src/views/home_view.dart';
 
@@ -17,22 +18,33 @@ class PersonalPortfolioApp extends StatelessWidget {
       designSize: const Size(1440, 944),
       minTextAdapt: true,
       splitScreenMode: true,
-      builder: (_, __) => BlocProvider<LocaleCubit>(
-        create: (_) => getIt.get<LocaleCubit>()..getSavedLang(),
-        child: BlocBuilder<LocaleCubit, Locale>(
-          buildWhen: (previous, current) => previous != current,
-          builder: (context, localeState) => MaterialApp(
-            debugShowCheckedModeBanner: false,
-            locale: localeState,
-            supportedLocales: AppLocalizationsSetup.supportedLocales,
-            localizationsDelegates:
-                AppLocalizationsSetup.localizationsDelegates,
-            localeResolutionCallback:
-                AppLocalizationsSetup.localeResolutionCallback,
-            title: AppStrings.appTitle,
-            theme: AppThemes.light,
-            // themeMode: AppThemes.light? ThemeMode.light : ThemeMode.dark,
-            home: const HomeView(),
+      builder: (_, __) => MultiBlocProvider(
+        providers: [
+          BlocProvider<LocaleCubit>(
+            create: (_) => getIt.get<LocaleCubit>()..getSavedLang(),
+          ),
+          BlocProvider<ThemingCubit>(
+            create: (_) => getIt.get<ThemingCubit>(),
+          ),
+        ],
+        child: BlocBuilder<ThemingCubit, ThemeData>(
+          builder: (_, themeState) => BlocBuilder<LocaleCubit, Locale>(
+            buildWhen: (previous, current) => previous != current,
+            builder: (_, localeState) => MaterialApp(
+              debugShowCheckedModeBanner: false,
+              locale: localeState,
+              supportedLocales: AppLocalizationsSetup.supportedLocales,
+              localizationsDelegates:
+                  AppLocalizationsSetup.localizationsDelegates,
+              localeResolutionCallback:
+                  AppLocalizationsSetup.localeResolutionCallback,
+              title: AppStrings.appTitle,
+              theme: themeState,
+              themeMode: themeState == AppThemes.light
+                  ? ThemeMode.light
+                  : ThemeMode.dark,
+              home: const HomeView(),
+            ),
           ),
         ),
       ),
