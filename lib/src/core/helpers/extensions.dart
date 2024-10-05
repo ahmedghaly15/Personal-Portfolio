806a5eb33e3dcc8636eaa88acdf1e3a294923236
Ahmed Ghaly <ahmedghaly0767@gmail.com>
2024-10-05 09:17:33 +0300

    Setuped Localization files

--- lib/src/core/helpers/extensions.dart
+++ lib/src/core/helpers/extensions.dart
@@ -4,3 +4,8 @@ extension MediaQueryExtension on BuildContext {
   double get height => MediaQuery.sizeOf(this).height;
   double get width => MediaQuery.sizeOf(this).width;
 }
+
+extension NullOrEmptyString on String? {
+  /// Check if the string is null or empty
+  bool get nullOrEmpty => this == null || this == '';
+}
---
+++ lib/src/core/utils/app_strings.dart
@@ -0,0 +1,8 @@
+class AppStrings {
+  AppStrings._();
+
+  static const String appTitle = 'Ahmed Ghaly';
+  static const String englishLangCode = 'en';
+  static const String arabicLangCode = 'ar';
+  static const String langPath = 'lib/src/core/locale/lang';
+}
