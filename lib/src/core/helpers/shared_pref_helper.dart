2d65756c2346931b9832af225dc9780056a855da
Ahmed Ghaly <ahmedghaly0767@gmail.com>
2024-10-05 08:57:26 +0300

    Built SharedPrefHelper

---
+++ lib/src/core/helpers/shared_pref_helper.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class SharedPrefHelper {
+  // private constructor as I don't want to allow creating an instance of this class itself.
+  SharedPrefHelper._();
+
+  /// Saves a [value] with a [key] in the SharedPreferences.
+  static Future<bool> setData(String key, value) async {
+    SharedPreferences sharedPreferences = await SharedPreferences.getInstance();
+    debugPrint("SharedPrefHelper : setData with key : $key and value : $value");
+    switch (value.runtimeType) {
+      case const (String):
+        return await sharedPreferences.setString(key, value);
+      case const (int):
+        return await sharedPreferences.setInt(key, value);
+      case const (bool):
+        return await sharedPreferences.setBool(key, value);
+      case const (double):
+        return await sharedPreferences.setDouble(key, value);
+      default:
+        return false;
+    }
+  }
+
+  /// Gets an String value from SharedPreferences with given [key].
+  static Future<String> getString(String key) async {
+    debugPrint('SharedPrefHelper : getString with key : $key');
+    SharedPreferences sharedPreferences = await SharedPreferences.getInstance();
+    return sharedPreferences.getString(key) ?? '';
+  }
+
+  /// Removes a value from SharedPreferences with given [key].
+  static removeData(String key) async {
+    debugPrint('SharedPrefHelper : data with key : $key has been removed');
+    SharedPreferences sharedPreferences = await SharedPreferences.getInstance();
+    await sharedPreferences.remove(key);
+  }
+
+  /// Removes all keys and values in the SharedPreferences
+  static clearAllData() async {
+    debugPrint('SharedPrefHelper : all data has been cleared');
+    SharedPreferences sharedPreferences = await SharedPreferences.getInstance();
+    await sharedPreferences.clear();
+  }
+}
