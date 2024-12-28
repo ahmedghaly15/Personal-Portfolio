679d6892a72a20437ccd8ead71fc85e4b2b13fd0
Ahmed Ghaly <ahmedghaly0767@gmail.com>
2024-12-28 07:17:39 +0200

    Built PassionAndPurposeSection & TechEnthusiastCard widgets  - Updated MainButton widget

--- lib/src/core/widgets/main_button.dart
+++ lib/src/core/widgets/main_button.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:personal_portfolio/src/core/themes/app_colors.dart';
 import 'package:personal_portfolio/src/core/themes/app_text_styles.dart';
 import 'package:personal_portfolio/src/core/utils/app_constants.dart';
 import 'package:personal_portfolio/src/core/utils/functions/get_responsive_font_size.dart';
@@ -17,6 +18,8 @@ class MainButton extends StatelessWidget {
     this.padding,
     this.fontSize,
     this.margin,
+    this.backgroundColor,
+    this.borderColor, this.gradient,
   });
 
   final String? textKey;
@@ -29,6 +32,8 @@ class MainButton extends StatelessWidget {
   final EdgeInsetsGeometry? padding;
   final double? fontSize;
   final EdgeInsetsGeometry? margin;
+  final Color? backgroundColor, borderColor;
+  final Gradient? gradient;
 
   @override
   Widget build(BuildContext context) {
@@ -38,12 +43,17 @@ class MainButton extends StatelessWidget {
       margin: margin,
       decoration: AppConstants.boxDecoration.copyWith(
         borderRadius: BorderRadius.circular(borderRadius ?? 13.r),
+        color: backgroundColor,
+        border: Border.all(
+          color: borderColor ?? AppColors.color6971A2.withAlpha(41),
+        ),
+        gradient: gradient?? AppConstants.boxPrimaryLinearGradient,
       ),
       child: MaterialButton(
         padding: padding ??
             EdgeInsets.symmetric(
               horizontal: 40.w,
-              vertical: 22.h,
+              vertical: 24.h,
             ),
         onPressed: onPressed,
         shape: RoundedRectangleBorder(
