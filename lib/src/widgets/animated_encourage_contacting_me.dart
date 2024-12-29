72a00bd1b5346c373353dae6b7ee6b2b3cd882e4
Ahmed Ghaly <ahmedghaly0767@gmail.com>
2024-12-29 15:18:50 +0200

    Updated LandingView & Refactored AnimatedEncourageContactingMe widget

--- lib/src/widgets/animated_encourage_contacting_me.dart
+++ lib/src/widgets/animated_encourage_contacting_me.dart
@@ -11,7 +11,9 @@ import 'package:personal_portfolio/src/core/widgets/main_button.dart';
 import 'package:visibility_detector/visibility_detector.dart';
 
 class AnimatedEncourageContactingMe extends StatefulWidget {
-  const AnimatedEncourageContactingMe({super.key});
+  const AnimatedEncourageContactingMe({super.key, this.contactMeButtonWidth});
+
+  final double? contactMeButtonWidth;
 
   @override
   State<AnimatedEncourageContactingMe> createState() =>
@@ -33,63 +35,80 @@ class _AnimatedEncourageContactingMeState
       },
       child: _isVisible
           ? ElasticIn(
-              child: Column(
-                spacing: 24.h,
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  RichText(
-                    textAlign: TextAlign.center,
-                    text: TextSpan(
-                      children: [
-                        TextSpan(
-                          text: '${AppStrings.lookingToBringYourIdeasToLife} ',
-                          style: AppTextStyles.font48Bold(context).copyWith(
-                            color: Colors.white,
-                          ),
-                        ),
-                        TextSpan(
-                          text: AppStrings.flutterApps,
-                          style: AppTextStyles.font48Bold(context).copyWith(
-                            color: AppColors.colorCBACF9,
-                          ),
-                        ),
-                        TextSpan(
-                          text: '?',
-                          style: AppTextStyles.font48Bold(context).copyWith(
-                            color: Colors.white,
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                  Text(
-                    AppStrings.reachMeOut,
-                    style: AppTextStyles.font16Regular(context),
-                    textAlign: TextAlign.center,
-                  ),
-                  MainButton(
-                    onPressed: () async => await openUrl(
-                      AppStrings.myGmail,
-                      isEmail: true,
-                    ),
-                    child: Row(
-                      spacing: 10.w,
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      mainAxisSize: MainAxisSize.min,
-                      children: [
-                        Text(
-                          AppStrings.contactMeNow,
-                          style: AppTextStyles.font18Medium(context),
-                        ),
-                        SvgPicture.asset(Assets.svgsLinkArrow),
-                      ],
-                    ),
-                  ),
-                ],
+              child: EncourageContactingMe(
+                contactMeButtonWidth: widget.contactMeButtonWidth,
               ),
             )
-          : const SizedBox.shrink(),
+          : const Opacity(
+              opacity: 0,
+              child: EncourageContactingMe(),
+            ),
+    );
+  }
+}
+
+class EncourageContactingMe extends StatelessWidget {
+  const EncourageContactingMe({super.key, this.contactMeButtonWidth});
+
+  final double? contactMeButtonWidth;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      spacing: 24.h,
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        RichText(
+          textAlign: TextAlign.center,
+          text: TextSpan(
+            children: [
+              TextSpan(
+                text: '${AppStrings.lookingToBringYourIdeasToLife} ',
+                style: AppTextStyles.font48Bold(context).copyWith(
+                  color: Colors.white,
+                ),
+              ),
+              TextSpan(
+                text: AppStrings.flutterApps,
+                style: AppTextStyles.font48Bold(context).copyWith(
+                  color: AppColors.colorCBACF9,
+                ),
+              ),
+              TextSpan(
+                text: '?',
+                style: AppTextStyles.font48Bold(context).copyWith(
+                  color: Colors.white,
+                ),
+              ),
+            ],
+          ),
+        ),
+        Text(
+          AppStrings.reachMeOut,
+          style: AppTextStyles.font16Regular(context),
+          textAlign: TextAlign.center,
+        ),
+        MainButton(
+          width: contactMeButtonWidth,
+          onPressed: () async => await openUrl(
+            AppStrings.myGmail,
+            isEmail: true,
+          ),
+          child: Row(
+            spacing: 10.w,
+            mainAxisAlignment: MainAxisAlignment.center,
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Text(
+                AppStrings.contactMeNow,
+                style: AppTextStyles.font18Medium(context),
+              ),
+              SvgPicture.asset(Assets.svgsLinkArrow),
+            ],
+          ),
+        ),
+      ],
     );
   }
 }
