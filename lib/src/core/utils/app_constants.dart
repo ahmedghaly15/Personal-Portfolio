40bb9518ad2906d40dd9da83183f9fac8b33af98
Ahmed Ghaly <ahmedghaly0767@gmail.com>
2024-12-29 08:48:43 +0200

    Added skills list to Skill model

--- lib/src/core/utils/app_constants.dart
+++ lib/src/core/utils/app_constants.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:personal_portfolio/src/core/themes/app_colors.dart';
 import 'package:personal_portfolio/src/core/utils/app_strings.dart';
-import 'package:personal_portfolio/src/models/skill.dart';
 
 class AppConstants {
   AppConstants._();
@@ -11,21 +10,6 @@ class AppConstants {
     AppStrings.about,
     AppStrings.skills,
     AppStrings.portfolio,
-  ];
-
-  static const List<Skill> skills = [
-    Skill(name: 'Dart', percent: 0.85),
-    Skill(name: 'OOP', percent: 0.8),
-    Skill(name: 'Flutter Framework', percent: 0.8),
-    Skill(name: 'Problem Solving', percent: 0.7),
-    Skill(name: 'Version Control (Git)', percent: 0.85),
-    Skill(name: 'UI/UX Implementation', percent: 0.8),
-    Skill(name: 'State Management (Cubit & Bloc)', percent: 0.9),
-    Skill(name: 'Firebase', percent: 0.8),
-    Skill(name: 'APIs Handling', percent: 0.9),
-    Skill(name: 'Clean Architecture', percent: 0.8),
-    Skill(name: 'MVVM Pattern', percent: 0.9),
-    Skill(name: 'Responsiveness', percent: 0.8),
   ];
 
   static BoxDecoration get boxDecoration => BoxDecoration(
--- lib/src/widgets/landing_view_desktop_skills_tab.dart
+++ lib/src/widgets/landing_view_desktop_skills_tab.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:personal_portfolio/src/core/themes/app_colors.dart';
 import 'package:personal_portfolio/src/core/themes/app_text_styles.dart';
 import 'package:personal_portfolio/src/core/utils/app_strings.dart';
+import 'package:personal_portfolio/src/widgets/landing_view_big_text.dart';
 import 'package:personal_portfolio/src/widgets/tabs_nav.dart';
 
 class LandingViewDesktopSkillsTab extends StatelessWidget {
@@ -21,10 +23,34 @@ class LandingViewDesktopSkillsTab extends StatelessWidget {
         SliverToBoxAdapter(
           child: Container(
             margin: EdgeInsets.only(top: 73.h, bottom: 22.h),
-            child: Text(
-              AppStrings.dynamicAppMagic,
-              style: AppTextStyles.font16Regular(context),
-              textAlign: TextAlign.center,
+            child: const HeaderSmallText(
+              text: AppStrings.masteringTheArtOfFlutter,
+            ),
+          ),
+        ),
+        SliverToBoxAdapter(
+          child: Align(
+            child: LandingViewBigText(
+              textSpans: [
+                TextSpan(
+                  text: '${AppStrings.elevating} ',
+                  style: AppTextStyles.font72Bold(context).copyWith(
+                    color: Colors.white,
+                  ),
+                ),
+                TextSpan(
+                  text: AppStrings.mySkills,
+                  style: AppTextStyles.font72Bold(context).copyWith(
+                    color: AppColors.colorCBACF9,
+                  ),
+                ),
+                TextSpan(
+                  text: '\n${AppStrings.oneLineCode}',
+                  style: AppTextStyles.font72Bold(context).copyWith(
+                    color: Colors.white,
+                  ),
+                ),
+              ],
             ),
           ),
         ),
