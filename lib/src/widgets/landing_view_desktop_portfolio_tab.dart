858db7a7105c3b7fbd6c0dd82175663f0e5c035d
Ahmed Ghaly <ahmedghaly0767@gmail.com>
2024-12-29 10:06:27 +0200

    Built DesktopProjectsSliverGrid widget & Finished LandingViewDesktopPortfolioTab widget

--- lib/src/widgets/landing_view_desktop_portfolio_tab.dart
+++ lib/src/widgets/landing_view_desktop_portfolio_tab.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:personal_portfolio/src/core/themes/app_text_styles.dart';
-import 'package:personal_portfolio/src/core/utils/app_strings.dart';
+import 'package:personal_portfolio/src/widgets/desktop_projects_sliver_grid.dart';
 import 'package:personal_portfolio/src/widgets/tabs_nav.dart';
 
 class LandingViewDesktopPortfolioTab extends StatelessWidget {
@@ -18,15 +17,9 @@ class LandingViewDesktopPortfolioTab extends StatelessWidget {
             child: TabsNav(),
           ),
         ),
-        SliverToBoxAdapter(
-          child: Container(
-            margin: EdgeInsets.only(top: 73.h, bottom: 22.h),
-            child: Text(
-              AppStrings.dynamicAppMagic,
-              style: AppTextStyles.font16Regular(context),
-              textAlign: TextAlign.center,
-            ),
-          ),
+        SliverPadding(
+          padding: EdgeInsets.symmetric(vertical: 56.h, horizontal: 100.w),
+          sliver: const DesktopProjectsSliverGrid(),
         ),
       ],
     );
