cf037f40a8905217e3f66fb5a5db1438ed0c8451
Ahmed Ghaly <ahmedghaly0767@gmail.com>
2024-12-30 11:39:07 +0200

    Updated firebase.json & pubspec files ✔  - Updated firebase hosting configurations  - Deleted get_it package because no need for it

--- lib/src/core/di/di.dart
+++
@@ -1,8 +0,0 @@
-import 'package:get_it/get_it.dart';
-import 'package:personal_portfolio/src/cubit/landing_cubit.dart';
-
-final GetIt getIt = GetIt.instance;
-
-void setupDI() {
-  getIt.registerLazySingleton<LandingCubit>(() => LandingCubit());
-}
