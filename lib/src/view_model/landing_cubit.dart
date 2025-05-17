83ca2c8f4c19c1b427df5c534a830478586a7175
ahmedghaly15 <ahmedghaly0767@gmail.com>
2025-05-17 21:38:05 +0300

    Updated Landing Cubit/State  - Handled fetching remote data

--- lib/src/view_model/landing_cubit.dart
+++ lib/src/view_model/landing_cubit.dart
@@ -1,9 +1,16 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+
+import '../core/utils/const_strings.dart';
+import '../models/fetch_data_response.dart';
 
 part 'landing_state.dart';
 
 class LandingCubit extends Cubit<LandingState> {
-  LandingCubit() : super(LandingState.initial());
+  final SupabaseClient _supabaseClient;
+
+  LandingCubit(this._supabaseClient) : super(LandingState.initial());
 
   void selectTabNav(int index) {
     if (state.selectedTabNavIndex != index) {
@@ -12,5 +19,33 @@ class LandingCubit extends Cubit<LandingState> {
         selectedTabNavIndex: index,
       ));
     }
+  }
+
+  void fetchData() async {
+    emit(state.copyWith(
+      status: LandingStatus.fetchDataLoading,
+    ));
+    try {
+      final FetchDataResponse data = await _fetchRemoteData();
+      emit(state.copyWith(
+        status: LandingStatus.fetchDataSuccess,
+        data: data,
+      ));
+    } catch (e) {
+      emit(state.copyWith(
+        status: LandingStatus.fetchDataError,
+        error: e.toString(),
+      ));
+    }
+  }
+
+  Future<FetchDataResponse> _fetchRemoteData() async {
+    final jsonData = await _supabaseClient
+        .from(ConstStrings.dataTable)
+        .select()
+        .eq('user_id', dotenv.env[ConstStrings.myIdKey]!)
+        .single();
+    final data = FetchDataResponse.fromJson(jsonData);
+    return data;
   }
 }
--- lib/src/view_model/landing_state.dart
+++ lib/src/view_model/landing_state.dart
@@ -3,24 +3,35 @@ part of 'landing_cubit.dart';
 enum LandingStatus {
   initial,
   selectTabNav,
+  fetchDataLoading,
+  fetchDataSuccess,
+  fetchDataError,
 }
 
 class LandingState {
   final LandingStatus status;
   final int selectedTabNavIndex;
+  final FetchDataResponse? data;
+  final String? error;
 
   const LandingState({
     required this.status,
     this.selectedTabNavIndex = 0,
+    this.data,
+    this.error,
   });
 
   LandingState copyWith({
     LandingStatus? status,
     int? selectedTabNavIndex,
+    FetchDataResponse? data,
+    String? error,
   }) =>
       LandingState(
         status: status ?? this.status,
         selectedTabNavIndex: selectedTabNavIndex ?? this.selectedTabNavIndex,
+        data: data ?? this.data,
+        error: error ?? this.error,
       );
 
   factory LandingState.initial() =>
