aa3f0d7cd9a98af56c743728bdf325d2d356a253
ahmedghaly15 <ahmedghaly0767@gmail.com>
2025-05-18 21:30:46 +0300

    Updated setupDI() func

--- lib/src/core/utils/functions/setup_di.dart
+++ lib/src/core/utils/functions/setup_di.dart
@@ -1,15 +1,28 @@
+import 'package:dio/dio.dart';
 import 'package:get_it/get_it.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
 import '../../../view_model/landing_cubit.dart';
-
+import '../../api/dio_factory.dart';
 
 final GetIt getIt = GetIt.instance;
 
-Future<void> setupDI() async {
+void _setupForExternal() {
   getIt.registerLazySingleton<SupabaseClient>(() => Supabase.instance.client);
-  getIt.registerFactory<LandingCubit>(
-    () => LandingCubit(getIt<SupabaseClient>()),
-  );
+}
 
+void _setupForCore() {
+  getIt.registerLazySingleton<Dio>(() => DioFactory.getDio());
+}
+
+void _setupForeCubits() {
+  getIt.registerFactory<LandingCubit>(
+    () => LandingCubit(getIt<SupabaseClient>(), getIt<Dio>()),
+  );
+}
+
+Future<void> setupDI() async {
+  _setupForExternal();
+  _setupForCore();
+  _setupForeCubits();
 }
