480f4d711099cd1e6c7a45b205275274ed03427d
Ahmed Ghaly <108659381+ahmedghaly15@users.noreply.github.com>
2025-06-11 19:54:05 +0300

    Moved Project model to external file

--- lib/src/models/about.dart
+++ lib/src/models/about.dart
@@ -1,6 +1,7 @@
 import 'package:json_annotation/json_annotation.dart';
 
 import '../core/enums/experience_status.dart';
+import 'project.dart';
 
 part 'about.g.dart';
 
@@ -40,25 +41,6 @@ class AboutHeaderTextModel {
       _$AboutHeaderTextModelFromJson(json);
 
   Map<String, dynamic> toJson() => _$AboutHeaderTextModelToJson(this);
-}
-
-@JsonSerializable()
-class Project {
-  final String imgPath, title, description;
-  final String? downloadLink, promoLink, gitHubLink;
-
-  Project({
-    required this.imgPath,
-    required this.title,
-    required this.description,
-    this.downloadLink,
-    this.promoLink,
-    this.gitHubLink,
-  });
-
-  factory Project.fromJson(Map<String, dynamic> json) =>
-      _$ProjectFromJson(json);
-  Map<String, dynamic> toJson() => _$ProjectToJson(this);
 }
 
 @JsonSerializable()
