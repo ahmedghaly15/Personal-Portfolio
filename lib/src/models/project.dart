63fc4718dbfba019877585d5fce13ab0c95c5d1b
Ahmed Ghaly <108659381+ahmedghaly15@users.noreply.github.com>
2025-06-11 20:01:09 +0300

    Built Project model

---
+++ lib/src/models/project.dart
@@ -0,0 +1,23 @@
+part 'part.g.dart';
+
+@JsonSerializable()
+class Project {
+  final String id, imgPath, title, description;
+  final String? downloadLink, promoLink, gitHubLink;
+  final bool shownInAbout;
+
+  Project({
+    required this.id,
+    required this.imgPath,
+    required this.title,
+    required this.description,
+    required this.shownInAbout,
+    this.downloadLink,
+    this.promoLink,
+    this.gitHubLink,
+  });
+
+  factory Project.fromJson(Map<String, dynamic> json) =>
+      _$ProjectFromJson(json);
+  Map<String, dynamic> toJson() => _$ProjectToJson(this);
+}
