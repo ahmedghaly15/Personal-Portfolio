fdf9f3096dfd6498988eea0936a515467d285695
Ahmed Ghaly <108659381+ahmedghaly15@users.noreply.github.com>
2025-06-27 01:54:12 +0300

    Updated AppAssets via Deplfolio: Deploy 🚀

--- lib/src/core/utils/app_assets.dart
+++ lib/src/core/utils/app_assets.dart
@@ -1,144 +1,37 @@
 // ignore_for_file: prefer_single_quotes
 class Assets {
   Assets._();
+  
+  /// Assets for fontsInterInter28ptBold
+  /// assets/fonts/Inter/Inter_28pt-Bold.ttf
+  static const String fontsInterInter28ptBold = "assets/fonts/Inter/Inter_28pt-Bold.ttf";
 
-  /// Assets for cvAhmed Ghaly  Flutter Dev
-  /// assets/cv/Ahmed Ghaly - Flutter Dev.pdf
-  static const String cvAhmedGhalyFlutterDev =
-      "assets/cv/Ahmed Ghaly - Flutter Dev.pdf";
+  /// Assets for fontsInterInter28ptMedium
+  /// assets/fonts/Inter/Inter_28pt-Medium.ttf
+  static const String fontsInterInter28ptMedium = "assets/fonts/Inter/Inter_28pt-Medium.ttf";
 
-  /// Assets for imagesBackgroundGridPattern
-  /// assets/images/background_grid-pattern.png
-  static const String imagesBackgroundGridPattern =
-      "assets/images/background_grid-pattern.png";
+  /// Assets for fontsInterInter28ptRegular
+  /// assets/fonts/Inter/Inter_28pt-Regular.ttf
+  static const String fontsInterInter28ptRegular = "assets/fonts/Inter/Inter_28pt-Regular.ttf";
 
-  /// Assets for imagesCentricGridPattern
-  /// assets/images/centric-grid-pattern.png
-  static const String imagesCentricGridPattern =
-      "assets/images/centric-grid-pattern.png";
-
-  /// Assets for imagesCopyEmailAddressBackground
-  /// assets/images/copy_email_address_background.png
-  static const String imagesCopyEmailAddressBackground =
-      "assets/images/copy_email_address_background.png";
-
-  /// Assets for imagesEndSpotlight
-  /// assets/images/end_spotlight.png
-  static const String imagesEndSpotlight = "assets/images/end_spotlight.png";
-
-  /// Assets for imagesFooterGridPattern
-  /// assets/images/footer-grid-pattern.png
-  static const String imagesFooterGridPattern =
-      "assets/images/footer-grid-pattern.png";
+  /// Assets for fontsInterInter28ptSemiBold
+  /// assets/fonts/Inter/Inter_28pt-SemiBold.ttf
+  static const String fontsInterInter28ptSemiBold = "assets/fonts/Inter/Inter_28pt-SemiBold.ttf";
 
   /// Assets for imagesFreelanceIcon
   /// assets/images/freelance-icon.png
   static const String imagesFreelanceIcon = "assets/images/freelance-icon.png";
 
-  /// Assets for imagesIcareIcon
-  /// assets/images/icare_icon.png
-  static const String imagesIcareIcon = "assets/images/icare_icon.png";
-
   /// Assets for imagesInternExperienceImg
   /// assets/images/intern-experience-img.png
-  static const String imagesInternExperienceImg =
-      "assets/images/intern-experience-img.png";
+  static const String imagesInternExperienceImg = "assets/images/intern-experience-img.png";
 
   /// Assets for imagesJobIcon
   /// assets/images/job-icon.png
   static const String imagesJobIcon = "assets/images/job-icon.png";
 
-  /// Assets for imagesLinkupIcon
-  /// assets/images/linkup_icon.png
-  static const String imagesLinkupIcon = "assets/images/linkup_icon.png";
-
-  /// Assets for imagesMobileGridPattern
-  /// assets/images/mobile-grid-pattern.png
-  static const String imagesMobileGridPattern =
-      "assets/images/mobile-grid-pattern.png";
-
-  /// Assets for imagesPrioritizeImage
-  /// assets/images/prioritize-image.png
-  static const String imagesPrioritizeImage =
-      "assets/images/prioritize-image.png";
-
-  /// Assets for imagesProjectItemBackground
-  /// assets/images/project-item-background.png
-  static const String imagesProjectItemBackground =
-      "assets/images/project-item-background.png";
-
-  /// Assets for imagesRoomeIcon
-  /// assets/images/roome_icon.png
-  static const String imagesRoomeIcon = "assets/images/roome_icon.png";
-
-  /// Assets for imagesStartSpotlight
-  /// assets/images/start_spotlight.png
-  static const String imagesStartSpotlight =
-      "assets/images/start_spotlight.png";
-
-  /// Assets for imagesStoreifyIcon
-  /// assets/images/storeify_icon.png
-  static const String imagesStoreifyIcon = "assets/images/storeify_icon.png";
-
-  /// Assets for imagesTechEnthusiastBackground
-  /// assets/images/tech-enthusiast-background.png
-  static const String imagesTechEnthusiastBackground =
-      "assets/images/tech-enthusiast-background.png";
-
-  /// Assets for imagesTechEnthusiastBoxImg
-  /// assets/images/tech-enthusiast-box-img.png
-  static const String imagesTechEnthusiastBoxImg =
-      "assets/images/tech-enthusiast-box-img.png";
-
-  /// Assets for lottieErrorAnimation
-  /// assets/lottie/error_animation.json
-  static const String lottieErrorAnimation =
-      "assets/lottie/error_animation.json";
-
-  /// Assets for lottieExperienceBackground
-  /// assets/lottie/experience_background.json
-  static const String lottieExperienceBackground =
-      "assets/lottie/experience_background.json";
-
-  /// Assets for lottieLoadingAnimation
-  /// assets/lottie/loading_animation.json
-  static const String lottieLoadingAnimation =
-      "assets/lottie/loading_animation.json";
-
-  /// Assets for lottieProjectBackgroundCircles
-  /// assets/lottie/project_background_circles.json
-  static const String lottieProjectBackgroundCircles =
-      "assets/lottie/project_background_circles.json";
-
-  /// Assets for svgsCopyIcon
-  /// assets/svgs/copy-icon.svg
-  static const String svgsCopyIcon = "assets/svgs/copy-icon.svg";
-
-  /// Assets for svgsDownloadIcon
-  /// assets/svgs/download-icon.svg
-  static const String svgsDownloadIcon = "assets/svgs/download-icon.svg";
-
-  /// Assets for svgsGithubIcon
-  /// assets/svgs/github-icon.svg
-  static const String svgsGithubIcon = "assets/svgs/github-icon.svg";
-
-  /// Assets for svgsInstagramIcon
-  /// assets/svgs/instagram-icon.svg
-  static const String svgsInstagramIcon = "assets/svgs/instagram-icon.svg";
-
-  /// Assets for svgsLinkArrow
-  /// assets/svgs/link_arrow.svg
-  static const String svgsLinkArrow = "assets/svgs/link_arrow.svg";
-
-  /// Assets for svgsLinkedInIcon
-  /// assets/svgs/linkedIn-icon.svg
-  static const String svgsLinkedInIcon = "assets/svgs/linkedIn-icon.svg";
-
-  /// Assets for svgsMediumIcon
-  /// assets/svgs/medium-icon.svg
-  static const String svgsMediumIcon = "assets/svgs/medium-icon.svg";
-
-  /// Assets for svgsPlay
-  /// assets/svgs/play.svg
-  static const String svgsPlay = "assets/svgs/play.svg";
+  /// Assets for lottieShyLottie
+  /// assets/lottie/shy-lottie.json
+  static const String lottieShyLottie = "assets/lottie/shy-lottie.json";
 }
+
